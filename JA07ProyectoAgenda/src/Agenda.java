a689326691a3292954700a12e0c55e3d78786f8e
pauessa <pauessa@pauessa-Lenovo-ideapad-310-15ISK>
2018-02-08 12:38:19 +0100

    Fin de copia

--- JA07ProyectoAgenda/src/Agenda.java
+++ JA07ProyectoAgenda/src/Agenda.java
@@ -9,8 +9,61 @@
  * @author pauessa
  */
 public class Agenda {
+
     private int anyo;
     private Pagina[] paginas;
     public Pagina abierta;
-    
+
+    public Agenda(int anyo) {
+        this.anyo = anyo;
+        if (anyo % 4 == 0) {
+            inicializa(true);
+        } else {
+            inicializa(false);
+        }
+    }
+
+    private void inicializa(boolean b) {
+        this.paginas = new Pagina[b ? 366 : 365];
+        int dia = 0;
+        for (int i = 0; i <= 31; i++) {
+            paginas[dia++] = new Pagina(i, 1);
+
+        }
+        for (int i = 0; i <= (b ? 29 : 28); i++) {
+            paginas[dia++] = new Pagina(i, 2);
+
+        }
+        for (int i = 0; i <= 31; i++) {
+            paginas[dia++] = new Pagina(i, 3);
+
+        }
+        for (int i = 0; i <= 30; i++) {
+            paginas[dia++] = new Pagina(i, 4);
+
+        }
+        for (int i = 0; i <= 31; i++) {
+            paginas[dia++] = new Pagina(i, 5);
+
+        }
+
+    }
+
+    public Pagina buscarPagina(int dia, int mes) {
+        Pagina busqueda = null, temp;
+        int cont = 0;
+        boolean encontrado = false;
+
+        while (cont < paginas.length && encontrado == false) {
+            temp = paginas[cont++];
+            if (temp.getDia() == dia && temp.getMes() == mes) {
+                busqueda = temp;
+                encontrado = true;
+            }
+
+        }
+        return busqueda;
+
+    }
+
 }
--- JA07ProyectoAgenda/src/Pagina.java
+++ JA07ProyectoAgenda/src/Pagina.java
@@ -27,11 +27,67 @@ public class Pagina {
 
     public void borrarCita(Cita c) {
         for (Cita cita : citas) {
-            if(cita.getHora()==c.getHora()&&cita.getMinutos()==c.getMinutos()){
+            if (cita.getHora() == c.getHora() && cita.getMinutos() == c.getMinutos()) {
                 this.citas.remove(cita);
             }
-            
+
         }
     }
+
+    public Cita buscarCita(int hora, int minutos) {
+        Cita busqueda = null, temp;
+        int cont = 0;
+        boolean encontrado = false;
+
+        while (cont < this.citas.size() && encontrado == false) {
+            temp = this.citas.get(cont++);
+            if (temp.getHora() == hora && temp.getMinutos() == minutos) {
+                busqueda = temp;
+                encontrado = true;
+            }
+        }
+        return busqueda;
+
+        /*de otra forma
+        for (Cita temp : this.citas) {
+            if(temp.getHora()==c.getHora()&& temp.getMinutos()==c.getMinutos()){
+                retunr temp;
+            }
+        }
+        return null;
+        
+         */
+    }
+    
+    public void leerPagina(){
+        StringBuilder st= new StringBuilder(this.getDia()+"/"+this.getMes()+"\n");
+        if(this.citas.isEmpty()){
+            st.append("la pagina esta en blanco.\n");
+        }else{
+            for (int i = 0; i < this.citas.size(); i++) {
+                st.append(this.citas.get(i).leerCita());
+                
+            }
+        }
+        System.out.println(st.toString());
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+    
+    
 
 }
